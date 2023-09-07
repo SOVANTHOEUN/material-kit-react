8ed0f53473283f8a41e0b50b45338de14b97578e
SOVANTHOEUN <sovanbunthoeunvoung@gmail.com>
2023-09-07 09:06:16 +0900

    hh

--- src/_mock/account.js
+++ src/_mock/account.js
@@ -1,7 +1,7 @@
 // ----------------------------------------------------------------------
 
 const account = {
-  displayName: ' 소완튼',
+  displayName: '소완튼',
   email: 'demo@minimals.cc',
   photoURL: '/assets/images/avatars/avatar_default.jpg',
 };
